d1499e90c431e42b2a70cfcf919b9d49600629be
alessandro <alessandro.fuda@gmail.com>
2017-12-05 16:41:18 +0000

    reset_password & migrations table renamed with lr_ prefix

--- database/migrations/2017_11_20_114241_add_is_admin_to_users_table.php
+++ database/migrations/2017_11_20_114241_add_is_admin_to_users_table.php
@@ -13,7 +13,7 @@ class AddIsAdminToUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('lr_users', function (Blueprint $table) {
             $table->boolean('is_admin')->default(0)->after('email');
         });
     }
@@ -25,7 +25,7 @@ class AddIsAdminToUsersTable extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('lr_users', function (Blueprint $table) {
             $table->dropColumn('is_admin');
         });
     }
--- database/migrations/2017_11_27_145125_add_is_actuator_to_users_table.php
+++ database/migrations/2017_11_27_145125_add_is_actuator_to_users_table.php
@@ -13,7 +13,7 @@ class AddIsActuatorToUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('lr_users', function (Blueprint $table) {
             $table->boolean('is_actuator')->default(0)->after('is_admin');
         });
     }
@@ -25,7 +25,7 @@ class AddIsActuatorToUsersTable extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('lr_users', function (Blueprint $table) {
             $table->dropColumn('is_actuator');
         });
     }
